563d1ee3535ef4ea15559e2d90df7ca3132d54db
lcy-wakalaka <403262194@qq.com>
2020-09-20 15:15:08 +0800

    创建

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <h2>AAAAAAAAAAAA</h2>
     </div>
     <router-view/>
   </div>
