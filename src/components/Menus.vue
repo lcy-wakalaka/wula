7dd27da9e581912d8539201c4e008c2f88f167c4
lcy-wakalaka <403262194@qq.com>
2020-09-30 14:10:20 +0800

    update

---
+++ src/components/Menus.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   HHH
+ </div>
+</template>
+
+<script>
+export default {
+  name: "Menus",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
